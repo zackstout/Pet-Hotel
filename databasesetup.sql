88feb2528cf9ca83a614553a58ab4687d4e24823
Zack Stout <zackstout@gmail.com>
2017-10-25 10:33:30 -0500

    added dbsetup file

---
+++ databasesetup.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE "hotel_owners" (
+"id" serial PRIMARY KEY,
+"first name" varchar(60) NOT NULL,
+"last name" varchar(60) NOT NULL
+);
+
+--INSERT INTO "hotel_owners" ("first name", "last name")
+--VALUES ('Michael', 'Vick');
+
+CREATE TABLE "hotel_pets" (
+"id" serial PRIMARY KEY,
+"owner_id" INT REFERENCES "hotel_owners",
+"name" varchar(40),
+"breed" varchar(40),
+"color" varchar(40)
+);
+--INSERT INTO "hotel_pets" ("name", "breed", "color")
+--VALUES ('Chuckles', 'pug', 'tan');
+
+CREATE TABLE "hotel_visits" (
+"id" serial PRIMARY KEY,
+"pet_id" INT REFERENCES "hotel_pets",
+"check-in" date,
+"check-out" date
+);
+--INSERT INTO "hotel_visits" ("check-in", "check-out")
+--VALUES ('1-2-2016', '1-5-2017');
--- server/rizoutes/dongleHotel_router.js
+++ server/rizoutes/dongleHotel_router.js
@@ -13,18 +13,18 @@ var config = {
 };
 var pool = new pg.Pool(config);
 
-dongleRouter.get('/week', function(req, res) {
-  pool.connect(function(errorConnectingToDb, db, done) {
-    if(errorConnectingToDb) {
-      console.log('Error connecting', errorConnectingToDb);
+dongleRouter.get('/', function(req, res) {
+  pool.connect(function(err, db, done) {
+    if(err) {
+      console.log('Error connecting', err);
       res.sendStatus(500);
     } else {
       //we connected to DB
       var queryText = 'SELECT * FROM "taskstodo" WHERE ("due" >= $1 AND "due" <= $2 AND "complete"=false);';
-      db.query(queryText, [startOfWeek, endOfWeek], function(errorMakingQuery, result){
+      db.query(queryText, [startOfWeek, endOfWeek], function(err, result){
         done();
-        if(errorMakingQuery) {
-          console.log('Error making query', errorMakingQuery);
+        if(err) {
+          console.log('Error making query', err);
           res.sendStatus(500);
         } else {
           res.send(result.rows);
