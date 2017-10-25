bc3a14a53ace065ea04516c87fe7d036e074624b
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-25 11:19:01 -0500

    partially built out functions for client js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,67 @@
 console.log('js');
 
-$(document).ready(f1);
+$(document).ready(readyNow);
 
-function f1() {
+function readyNow() {
   console.log('jq');
+
+$('form').on('submit', function (event){
+  event.preventDefault();
+  registerOwner();
+}); // prevents automatic refresh due to form and calls register function
+$('#addPetBtn').on('click', addPet); // calls addPet function
+
+}
+
+function refreshPets() {
+  console.log ('refreshing Pets');
+
+  $.ajax({
+    type: 'GET',
+    url: '/pets',
+  }).done(function(response){
+    console.log(response);
+    var pets = response;
+    console.log(pets);
+    appendToDom(pets);
+  }).fail(function (error){
+    alert('something went wrong');
+  });
+} // end refreshPets function
+
+function registerOwner() {
+  console.log('registerOwner clicked');
+  var firstNameIn = $('#firstName').val();
+  var lastNameIn = $('#lastName').val();
+
+  var ownerObjectToSend = {
+    first_name: firstNameIn,
+    last_name: lastNameIn
+  };
+   $.ajax({
+      url: '/pets',
+      type: 'POST',
+      data: ownerObjectToSend
+   }).done(function (response) {
+   refreshOwners();
+   }).fail(function (error) {
+     console.log('error', error);
+   });
+}  // end registerOwner function
+
+function refreshOwners() {
+  
+}
+
+function addPet() {
+  console.log('addPet clicked');
+  var ownerNameIn = o
+  var petNameIn = $('#petName').val();
+  var petColorIn = $('#petColor').val();
+  var petBreedIn = $('#petBreed').val();
+
+  var newPetObjectToSend = {
+    
+    
+  }
 }
