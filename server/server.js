15dfad12367f101ccd035ceb6c759dca3d9a6963
Zack Stout <zackstout@gmail.com>
2017-10-25 10:00:40 -0500

    server up and running...

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// var dongleHotelRouter = require('./routes/dongleHotel_router.js');
+
+var app = express();
+var port = process.env.PORT || 5050;
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/hotel', dongleHotelRouter);
+
+app.listen(port, function() {
+  console.log('thx for listening on channel', port);
+});
